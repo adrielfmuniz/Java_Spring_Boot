23ba18ccc0b65dc9313c734f53699ba4315ff10e
Adriel Muniz <adriel@gmx.com>
2021-07-23 23:47:55 -0300

    Delete #2 - Inicializr-Mavan-H2-Hibernate directory

--- #2 - Inicializr-Mavan-H2-Hibernate/src/main/java/com/acc/contatos/repository/Contatos.java
+++
@@ -1,9 +0,0 @@
-package com.acc.contatos.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.acc.contatos.model.Contato;
-
-public interface Contatos extends JpaRepository<Contato, Long> {
-
-}
